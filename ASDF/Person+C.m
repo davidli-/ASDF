0f6e6a7475ad731d11f898618619f4029119abcb
davidli- <macmafia@sina.cn>
2019-01-11 00:40:36 +0800

    分类与属性
    分类中的属性通过运行时API可以获取到；
    
    但分类.h和.m文件中不能同时存在以下定义：
    @interface Person(C)
    ...
    @end
    
    会报警告"Duplicate definition of category 'C' on interface 'Person'"！
    
    如果同时在这两个定义中声明属性，那么只能获取到.m文件中这个定义中的属性~

---
+++ ASDF/Person+C.m
@@ -0,0 +1,17 @@
+//
+//  Person+C.m
+//  ASDF
+//
+//  Created by Macmafia on 2019/1/10.
+//  Copyright © 2019 Macmafia. All rights reserved.
+//
+
+#import "Person+C.h"
+
+@interface Person(C)
+@property(nonatomic) int Cat2;
+@end
+
+@implementation Person (C)
+
+@end
